fccda498a132e8403c5f1a34228de707bf6cb904
Fiasco <horaciomota93@gmail.com>
2024-05-30 22:16:13 +0100

    first commit

---
+++ src/test/java/dev/horacemota/Bank/System/BankSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.horacemota.Bank.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
