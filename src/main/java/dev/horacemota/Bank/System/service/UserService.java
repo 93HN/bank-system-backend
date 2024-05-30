166ce2320519b14b4f701af18a1e81aa8593be37
Fiasco <horaciomota93@gmail.com>
2024-05-30 23:27:50 +0100

    feat: Service is implementing userService now

---
+++ src/main/java/dev/horacemota/Bank/System/service/UserService.java
@@ -0,0 +1,8 @@
+package dev.horacemota.Bank.System.service;
+
+import dev.horacemota.Bank.System.entity.User;
+
+public interface UserService {
+    public User registerUser(User user);
+
+}
---
+++ src/main/java/dev/horacemota/Bank/System/service/UserServiceImp.java
@@ -0,0 +1,20 @@
+package dev.horacemota.Bank.System.service;
+import dev.horacemota.Bank.System.entity.User;
+import dev.horacemota.Bank.System.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    private final UserRepository repository;
+
+    @Autowired
+    public UserServiceImp(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public User registerUser(User user) {
+        return repository.save(user);
+    }
+}
