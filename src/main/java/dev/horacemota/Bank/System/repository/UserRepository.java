6f792f32b8bda4f2f99ba757618c838bb9ee61a2
Fiasco <horaciomota93@gmail.com>
2024-05-30 23:04:30 +0100

    feat: User repository created

---
+++ src/main/java/dev/horacemota/Bank/System/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dev.horacemota.Bank.System.repository;
+
+import dev.horacemota.Bank.System.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
